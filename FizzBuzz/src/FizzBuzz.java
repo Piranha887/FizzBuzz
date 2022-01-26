70b71fc767ad2495e377d527b81c67f2e8efe491
Piranha887 <98482618+Piranha887@users.noreply.github.com>
2022-01-26 20:45:08 +0300

    Add files via upload

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,12 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        System.out.println("Числа от одного до ста:");
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 & i % 5 == 0) System.out.println("FizzBuzz");
+            else if (i % 5 == 0) System.out.println("Buzz");
+            else if (i % 3 == 0) System.out.println("Fizz");
+            else System.out.println(i);
+        }
+    }
+}
